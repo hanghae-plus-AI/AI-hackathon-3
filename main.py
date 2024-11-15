34d8d720769407fbb03a22ca145d001b07163943
eunsung <edplab2019@gmail.com>
2024-11-15 21:50:07 +0900

    기본 라우터 /ai/{endpoint}

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 
 app = FastAPI()
 
-@app.get('/healthcheck')
+ai_router = APIRouter(
+    prefix='/ai',
+    tags = ['AI'],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@ai_router.get('/healthcheck')
 def healthcheck():
     return {'status': 'ok'}
 
+app.include_router(ai_router)
 
 if __name__ == '__main__':
     import uvicorn
+    
     uvicorn.run(app, host='0.0.0.0', port=8000)
